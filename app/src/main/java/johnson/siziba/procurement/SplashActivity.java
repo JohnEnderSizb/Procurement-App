66f0381ebc13bbfabf04c3bbdfb3668e4617843e
Johnson A.Siziba <siziba.uz@gmail.com>
2020-02-20 15:14:02 +0200

    Initial commit

---
+++ app/src/main/java/johnson/siziba/procurement/SplashActivity.java
@@ -0,0 +1,38 @@
+package johnson.siziba.procurement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getActionBar().hide();
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
